e1339dda64ffbd07568d3e2b6e0960bc97b93571
janst <jan.starc@gmail.com>
2020-11-07 13:12:04 +0100

    Google Cloud config files added

--- src/main/java/com/jan/ad_mediation_challenge/repository/SqlDAO.java
+++ src/main/java/com/jan/ad_mediation_challenge/repository/SqlDAO.java
@@ -1,7 +1,6 @@
 package com.jan.ad_mediation_challenge.repository;
 
 import com.jan.ad_mediation_challenge.domain.PerformanceData;
-import com.joutvhu.dynamic.jpa.DynamicQuery;
 import com.joutvhu.dynamic.jpa.support.DynamicJpaRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
